92bd2789ab2ea3682e7b74d58d2291fa1590d769
Zeus Script <51821404+Z-eus@users.noreply.github.com>
2025-04-27 18:36:25 +0300

    Added 3 new animations and eat&drink animation prop placement, rotation fixed.

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ __Hud redesigned by Z-eus__
 - **Metabolism System**: Manages hunger, thirst, and metabolism stats, with configurable rates for each.
 - **Consumable Items**: Items such as food and drinks that restore thirst, hunger, and stamina.
 - **Effects System**: Allows application of visual effects (e.g., `PlayerDrunkSaloon1`) when players consume certain items. 
-- **Configurable Animations**: Each consumable has its associated animation (e.g., drink, eat).
+- **Configurable Animations**: Each consumable has its associated animation drink/eat/stew/syringe/bandage
 - **Give Back Item:** Item given back to the player after consumable
 
 ## Configuration
@@ -58,7 +58,7 @@ To add a new consumable item, follow this structure:
     InnerCoreHealth = 50,                -- How much inner core health is affected.
     OuterCoreHealth = 25,                -- How much outer core health is affected.
     PropName = "prop_name",              -- The in-game prop to display while using the item.
-    Animation = "eat/drink",             -- The animation played when consuming the item.
+    Animation = "animation",             -- The animation the player will use when consuming this item drink/eat/stew/syringe/bandage
     Effect = "effect_name",              -- (Optional) Any visual effect applied when consuming the item.
     EffectDuration = 1,                  -- (Optional) Duration of the effect in minutes.
     GiveBackItemLabel = "item_label",    -- The display name of the GiveBackItem.
--- client/useItemsActions.lua
+++ client/useItemsActions.lua
@@ -85,8 +85,14 @@ RegisterNetEvent('vorpmetabolism:useItem', function(index, label)
 
     if (Config["ItemsToUse"][index]["Animation"] == "eat") then
         PlayAnimEat(Config["ItemsToUse"][index]["PropName"])
-    else
+    elseif (Config["ItemsToUse"][index]["Animation"] == "stew") then
+        PlayAnimStew(Config["ItemsToUse"][index]["PropName"])
+    elseif (Config["ItemsToUse"][index]["Animation"] == "drink") then
         PlayAnimDrink(Config["ItemsToUse"][index]["PropName"])
+    elseif (Config["ItemsToUse"][index]["Animation"] == "syringe") then
+        PlayAnimSyringe(Config["ItemsToUse"][index]["PropName"])
+    elseif (Config["ItemsToUse"][index]["Animation"] == "bandage") then
+        PlayAnimBandage(Config["ItemsToUse"][index]["PropName"])
     end
 
     if (Config["ItemsToUse"][index]["Effect"] ~= "") then
@@ -97,12 +103,77 @@ end)
 function ScreenEffect(effect, durationMinutes)
     local durationMilliseconds = durationMinutes * 60000 -- Convert minutes to milliseconds
     AnimpostfxPlay(effect)
-    Citizen.Wait(durationMilliseconds)
+    Wait(durationMilliseconds)
     AnimpostfxStop(effect)
 end
 
+function PlayAnimBandage(propName)
+    local playerCoords = GetEntityCoords(PlayerPedId())
+    local dict = "mini_games@story@mob4@heal_jules@bandage@arthur"
+    local anim = "tourniquet_slow"
+
+    RequestAnimDict(dict)
+    while (not HasAnimDictLoaded(dict)) do
+        Wait(100)
+    end
+
+    local hashItem = GetHashKey(propName)
+
+    local prop = CreateObject(hashItem, playerCoords.x, playerCoords.y, playerCoords.z, true, true, false)
+    local boneIndex = GetEntityBoneIndexByName(PlayerPedId(), "SKEL_L_HAND")
+
+    Wait(1000)
+
+    TaskPlayAnim(PlayerPedId(), dict, anim, 1.0, 8.0, 5000, 31, 0.0, false, false, false)
+    AttachEntityToEntity(prop, PlayerPedId(), boneIndex, 0.10, 0.0, 0.03, 0.0, -60.0, -90.0, true, true, false, true, 1, true)
+    Wait(6000)
+
+    DeleteObject(prop)
+    ClearPedSecondaryTask(PlayerPedId())
+end
+
+function PlayAnimSyringe(propName)
+    local playerCoords = GetEntityCoords(PlayerPedId())
+    local dict = "mech_inventory@item@stimulants@inject@quick"
+    local anim = "quick_stimulant_inject_lhand"
+
+    RequestAnimDict(dict)
+    while (not HasAnimDictLoaded(dict)) do
+        Wait(100)
+    end
+
+    local hashItem = GetHashKey(propName)
+
+    local prop = CreateObject(hashItem, playerCoords.x, playerCoords.y, playerCoords.z, true, true, false)
+    local boneIndex = GetEntityBoneIndexByName(PlayerPedId(), "SKEL_L_HAND")
+
+    Wait(1000)
+
+    TaskPlayAnim(PlayerPedId(), dict, anim, 1.0, 8.0, 5000, 31, 0.0, false, false, false)
+    AttachEntityToEntity(prop, PlayerPedId(), boneIndex, 0.10, 0.0, 0.03, 0.0, -80.0, -90.0, true, true, false, true, 1, true)
+    Wait(2000) -- 6000
+
+    DeleteObject(prop)
+    ClearPedSecondaryTask(PlayerPedId())
+end
+
+function PlayAnimStew(propName)
+    local playerPed = PlayerPedId()
+    local playerCoords = GetEntityCoords(playerPed)
+    local stewProp = CreateObject(propName, playerCoords.x, playerCoords.y, playerCoords.z, true, true, false)
+    local stewSpoonProp = CreateObject("p_beefstew_spoon01x", playerCoords.x, playerCoords.y, playerCoords.z, true, true, false)
+    Citizen.InvokeNative(0x669655FFB29EF1A9, stewProp, 0, "Stew_Fill", 1.0)
+    Citizen.InvokeNative(0xCAAF2BCCFEF37F77, stewProp, 20)
+    Citizen.InvokeNative(0xCAAF2BCCFEF37F77, stewSpoonProp, 82)
+    -- https://github.com/femga/rdr3_discoveries/blob/ab8efca7aaf8f3e3b3b5a76893ac4787ad676dbf/animations/scenarios/scenario_attached_props.lua
+    TaskItemInteraction_2(playerPed, 599184882, stewProp, joaat("p_bowl04x_stew_PH_L_HAND"), -583731576, 1, 0, -1.0)
+    TaskItemInteraction_2(playerPed, 599184882, stewSpoonProp, joaat("p_spoon01x_PH_R_HAND"), -583731576, 1, 0, -1.0)
+    -- https://github.com/femga/rdr3_discoveries/tree/master/tasks/TASK_ITEM_INTERACTION#-4-item_interaction_state_name--item_interaction_propid--1
+    Citizen.InvokeNative(0xB35370D5353995CB, playerPed, -583731576, 1.0)
+end
+
 function PlayAnimDrink(propName)
-    local playerCoords = GetEntityCoords(PlayerPedId(), true, true)
+    local playerCoords = GetEntityCoords(PlayerPedId())
     local dict = "amb_rest_drunk@world_human_drinking@male_a@idle_a"
     local anim = "idle_a"
 
@@ -118,23 +189,22 @@ function PlayAnimDrink(propName)
 
     local hashItem = GetHashKey(propName)
 
-    local prop = CreateObject(hashItem, playerCoords.x, playerCoords.y, playerCoords.z + 0.2, true, true, false, false,
-        true)
-    local boneIndex = GetEntityBoneIndexByName(PlayerPedId(), "SKEL_R_Finger12")
+    local prop = CreateObject(hashItem, playerCoords.x, playerCoords.y, playerCoords.z, true, true, false)
+    local boneIndex = GetEntityBoneIndexByName(PlayerPedId(), "SKEL_R_HAND") -- SKEL_R_Finger12
 
     Wait(1000)
 
     TaskPlayAnim(PlayerPedId(), dict, anim, 1.0, 8.0, 5000, 31, 0.0, false, false, false)
-    AttachEntityToEntity(prop, PlayerPedId(), boneIndex, 0.02, 0.028, 0.001, 15.0, 175.0, 0.0, true, true, false, true, 1,
-        true)
-    Wait(6000)
+    AttachEntityToEntity(prop, PlayerPedId(), boneIndex, 0.08, -0.04, -0.05, -75.0, 0.0, 0.0, true, true, false, true, 1, true)
+    -- AttachEntityToEntity(prop, PlayerPedId(), boneIndex, 0.02, -0.018, 0.10, 15.0, 175.0, 0.0, true, true, false, true, 1, true)
+    Wait(5300) -- 6000
 
     DeleteObject(prop)
     ClearPedSecondaryTask(PlayerPedId())
 end
 
 function PlayAnimEat(propName)
-    local playerCoords = GetEntityCoords(PlayerPedId(), true, true)
+    local playerCoords = GetEntityCoords(PlayerPedId())
     local dict = "mech_inventory@clothing@bandana"
     local anim = "NECK_2_FACE_RH"
 
@@ -150,22 +220,21 @@ function PlayAnimEat(propName)
 
     local hashItem = GetHashKey(propName)
 
-    local prop = CreateObject(hashItem, playerCoords.x, playerCoords.y, playerCoords.z + 0.2, true, true, false, false,
-        true)
-    local boneIndex = GetEntityBoneIndexByName(PlayerPedId(), "SKEL_R_Finger12")
+    local prop = CreateObject(hashItem, playerCoords.x, playerCoords.y, playerCoords.z + 0.2, true, true, false)
+    local boneIndex = GetEntityBoneIndexByName(PlayerPedId(), "SKEL_R_HAND") -- SKEL_R_Finger12
 
     Wait(1000)
 
     TaskPlayAnim(PlayerPedId(), dict, anim, 1.0, 8.0, 5000, 31, 0.0, false, false, false)
-    AttachEntityToEntity(prop, PlayerPedId(), boneIndex, 0.02, 0.028, 0.001, 15.0, 175.0, 0.0, true, true, false, true, 1,
-        true)
-    Wait(6000)
+    AttachEntityToEntity(prop, PlayerPedId(), boneIndex, 0.08, -0.04, -0.05, -75.0, 0.0, 0.0, true, true, false, true, 1, true)
+    -- AttachEntityToEntity(prop, PlayerPedId(), boneIndex, 0.02, 0.028, 0.001, 15.0, 175.0, 0.0, true, true, false, true, 1, true)
+    Wait(5300) -- 6000
 
     DeleteObject(prop)
     ClearPedSecondaryTask(PlayerPedId())
 end
 
---on resource start
+-- On Resource Start
 AddEventHandler('onResourceStart', function(resourceName)
     if Config.DevMode then
         if (GetCurrentResourceName() == resourceName) then
